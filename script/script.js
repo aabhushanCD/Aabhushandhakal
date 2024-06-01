4b12d533d52e49dcac857140afbb1ed507445e8b
aabhushanCD <dammardhakal426@gmail.com>
2024-06-01 21:59:56 +0545

    ui

--- script/script.js
+++ script/script.js
@@ -1,23 +1,9 @@
 let btm;
 
-function Event(url, Id) {
-  btm = document.getElementById(Id);
-  btm.addEventListener("click", function () {
-    // Use window.location.href to navigate to the specified URL
-    window.location.href = url;
-  });
-}
 
-Event("index.html", "btm1");
-Event("Contact.html", "btm2");
-Event("AboutMe.html", "btm3");
-Event("Interested.html", "btm4");
 
 var menuElements = document.getElementsByClassName("Menu");
 
-for (var i = 0; i < menuElements.length; i++) {
-  menuElements[i].style.display = "none";
-}
 
 function menulogo() {
   var newDiv = document.getElementById("Menu_logo");
