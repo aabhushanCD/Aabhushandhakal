d7660fd666a6c1f0f59ac3b242ce420129f11a3f
aabhushanCD <dammardhakal426@gmail.com>
2024-06-03 17:25:58 +0545

    dklfja

--- script/script.js
+++ script/script.js
@@ -17,6 +17,24 @@ function Menu() {
   }
 }
 
+// Read more buttom 
+document.getElementById('readMoreBtn').addEventListener('click', function() {
+  var moreText = document.querySelector('.aboutMe_Text .more-text');
+  var shortText = document.querySelector('.aboutMe_Text .short-text');
+  var btnText = document.getElementById('readMoreBtn');
+
+  if (moreText.style.display === 'none') {
+    moreText.style.display = 'block';
+    shortText.style.display = 'none';
+    btnText.innerHTML = 'Read Less';
+  } else {
+    moreText.style.display = 'none';
+    shortText.style.display = 'block';
+    btnText.innerHTML = 'Read More';
+  }
+});
+
+
 // slider js
 
 // var counter = 1;
