d1a1a6178af1c4374514ecd3749fcb67eaaf3564
aabhushanCD <dammardhakal426@gmail.com>
2025-04-07 11:59:30 +0545

    nav responsive

--- script/script.js
+++ script/script.js
@@ -2,5 +2,6 @@ const menuToggle = document.querySelector(".menu-toggle");
 const navLinks = document.querySelector(".nav-links");
 
 menuToggle.addEventListener("click", () => {
+  console.log("hellow");
   navLinks.classList.toggle("show");
 });
