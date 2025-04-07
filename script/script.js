36451b67b2c8ae30ce358df5e69e051cf052a7da
aabhushanCD <dammardhakal426@gmail.com>
2025-04-07 11:20:42 +0545

    portfolio add design only

--- script/script.js
+++ script/script.js
@@ -1,50 +1,6 @@
-let btm;
-var menuElements = document.getElementsByClassName("Menu");
-function menulogo() {
-  var newDiv = document.getElementById("Menu_logo");
-  if (newDiv) {
-    newDiv.style.display = "none";
-    return `logoIsRemoved`;
-  }
-}
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
 
-function Menu() {
-  btm = document.getElementsByClassName("Menu");
-  if (menulogo() === "logoIsRemoved") {
-    for (var i = 0; i < menuElements.length; i++) {
-      menuElements[i].style.display = "block";
-    }
-  }
-}
-
-// Read more buttom
-document.getElementById("readMoreBtn").addEventListener("click", function () {
-  var moreText = document.querySelector(".aboutMe_Text .more-text");
-  var shortText = document.querySelector(".aboutMe_Text .short-text");
-  var btnText = document.getElementById("readMoreBtn");
-
-  if (moreText.style.display === "none") {
-    moreText.style.display = "block";
-    shortText.style.display = "none";
-    btnText.innerHTML = "Read Less";
-  } else {
-    moreText.style.display = "none";
-    shortText.style.display = "block";
-    btnText.innerHTML = "Read More";
-  }
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
 });
-
-function remove_menu() {
-  let menu = document.getElementsByClassName("Menu");
-  menu.style.display = none;
-}
-// slider js
-
-// var counter = 1;
-// setInterval(() => {
-//   document.getElementById("radio" + counter).checked = true;
-//   counter++;
-//   if (counter > 4) {
-//     counter = 1;
-//   }
-// }, 2000);
