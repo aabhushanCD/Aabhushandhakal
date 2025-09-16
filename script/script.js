0e9b41255e4d825d06f5b1cd3ba3a947d0e711b0
aabhushanCD <dammardhakal426@gmail.com>
2025-09-16 12:58:36 +0545

    refactor

--- script/script.js
+++ script/script.js
@@ -1,7 +1,1 @@
-const menuToggle = document.querySelector(".menu-toggle");
-const navLinks = document.querySelector(".nav-links");
 
-menuToggle.addEventListener("click", () => {
-  console.log("hellow");
-  navLinks.classList.toggle("show");
-});
